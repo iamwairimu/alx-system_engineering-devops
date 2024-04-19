fcf82294540f43f2ee20d21531d1e254df21cb3c
iamwairimu <kmnleah@gmail.com>
2024-04-19 12:51:43 -0700

    requirement

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
+# Ensure 'pip3' is installed
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
 #install flask from pip3
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
